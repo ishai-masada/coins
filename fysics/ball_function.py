b88e5fa24260d9cf56c7e7bd927759e730a31d66
Ishai 'Doofus' Masada <imasada@vtcap.org>
2021-05-19 08:32:08 -0400

    init

---
+++ fysics/ball_function.py
@@ -0,0 +1,51 @@
+import math
+
+class Vector2:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+def ball_throw(ang, speed):
+    """ angle is in degrees and speed in meters/second"""
+    
+    fysics_dict = dict()
+    # Convert angle to radians
+    converted_ang = (ang * math.pi) / 180
+
+    # Get the vertical speed of the ball
+    v_speed = speed * math.sin(converted_ang)
+    
+    acceleration = 9.8
+    
+    # calculate the time it takes for ball to land
+    time = round(2 * v_speed/acceleration, 2)
+
+    # Check to see if trig functuion made time negative
+    if time < 0:
+        time = -1 * time
+
+    # Use the range equation to find the distance
+    distance = round((speed**2 * math.sin(2*converted_ang))/ acceleration, 2)
+
+    # Put time and distance into dictionary
+    fysics_dict["distance"] = distance
+    fysics_dict["time"] = time
+
+    return fysics_dict
+
+
+def simulated_ball_throw(ang, speed):
+    steps_per_sec = 100
+    position = Vector2()
+    converted_ang = (ang * math.pi) / 180
+    v_speed = speed * math.sin(converted_ang)
+    h_speed = speed * math.cos(converted_ang)
+    velocity = Vector2(v_speed, h_speed)
+    
+    while True:
+        position.x += velocity.x/steps_per_sec
+        position.y += velocity.y/steps_per_sec
+        velocity.y -= 9.8/steps_per_sec
+        if position.y < 0:
+            return abs(position.x)
+    
